85f77a1fc4f6dbe2cd91979fba9372530a103036
Brian Semrau <brian@geekkidconsulting.com>
2016-12-19 14:48:22 -0600

    Added loading basic config into memory

--- CoreInstall/CoreInstall/Form1.Designer.cs
+++ CoreInstall/CoreInstall/Form1.Designer.cs
@@ -32,18 +32,18 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.checkBox3 = new System.Windows.Forms.CheckBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -52,9 +52,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(13, 13);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(174, 13);
+            this.label1.Size = new System.Drawing.Size(316, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Please enter your sync-server URL:";
+            this.label1.Text = "Please enter your sync-server URL (do not specify port right now):";
             // 
             // textBox1
             // 
@@ -93,25 +93,81 @@
             this.panel1.TabIndex = 3;
             this.panel1.Visible = false;
             // 
-            // checkBox1
+            // button2
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(3, 3);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(80, 17);
-            this.checkBox1.TabIndex = 0;
-            this.checkBox1.Text = "Sync library";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.button2.Location = new System.Drawing.Point(6, 196);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 36);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Log in and begin setup";
+            this.button2.UseVisualStyleBackColor = true;
             // 
-            // checkBox2
+            // textBox5
             // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(90, 3);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(81, 17);
-            this.checkBox2.TabIndex = 1;
-            this.checkBox2.Text = "Sync media";
-            this.checkBox2.UseVisualStyleBackColor = true;
+            this.textBox5.Location = new System.Drawing.Point(6, 169);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(499, 20);
+            this.textBox5.TabIndex = 10;
+            this.textBox5.UseSystemPasswordChar = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 153);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Email address:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(6, 130);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(499, 20);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.UseSystemPasswordChar = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Full name:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(6, 87);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(499, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.UseSystemPasswordChar = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Password:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 44);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(499, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Username:";
             // 
             // checkBox3
             // 
@@ -123,81 +179,25 @@
             this.checkBox3.Text = "Sync preferences/playlists";
             this.checkBox3.UseVisualStyleBackColor = true;
             // 
-            // label2
+            // checkBox2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(3, 27);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(58, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Username:";
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(90, 3);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(81, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Sync media";
+            this.checkBox2.UseVisualStyleBackColor = true;
             // 
-            // textBox2
+            // checkBox1
             // 
-            this.textBox2.Location = new System.Drawing.Point(6, 44);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(499, 20);
-            this.textBox2.TabIndex = 4;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(5, 71);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(56, 13);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Password:";
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(6, 87);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(499, 20);
-            this.textBox3.TabIndex = 6;
-            this.textBox3.UseSystemPasswordChar = true;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(7, 114);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(55, 13);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "Full name:";
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(6, 130);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(499, 20);
-            this.textBox4.TabIndex = 8;
-            this.textBox4.UseSystemPasswordChar = true;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(7, 153);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(75, 13);
-            this.label5.TabIndex = 9;
-            this.label5.Text = "Email address:";
-            // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(6, 169);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(499, 20);
-            this.textBox5.TabIndex = 10;
-            this.textBox5.UseSystemPasswordChar = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(6, 196);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(133, 36);
-            this.button2.TabIndex = 11;
-            this.button2.Text = "Log in and begin setup";
-            this.button2.UseVisualStyleBackColor = true;
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(3, 3);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Sync library";
+            this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
--- CoreInstall/CoreInstall/Form1.cs
+++ CoreInstall/CoreInstall/Form1.cs
@@ -16,6 +16,15 @@ namespace CoreInstall
     public partial class Form1 : Form
     {
         string config = "";
+
+        string dns = "";
+        string mediarepo = "";
+        string libraryrepo = "";
+        string prefrepo = "";
+
+        Boolean syncmedia = true;
+        Boolean synclibrary = true;
+        Boolean syncpref = true;
         public Form1()
         {
             InitializeComponent();
@@ -51,11 +60,101 @@ namespace CoreInstall
                     panel1.Visible = true;
                     button1.Enabled = false;
                     textBox1.Enabled = false;
+
+                    if (Boolean.Parse(SubstringExtensions.Between(config, "<synclibrary>", @"</synclibrary>")))
+                    {
+                        checkBox1.Checked = true;
+                    } else
+                    {
+                        checkBox1.Checked = false;
+                        checkBox1.Enabled = false;
+                    }
+                    if (Boolean.Parse(SubstringExtensions.Between(config, "<syncmedia>", @"</syncmedia>")))
+                    {
+                        checkBox2.Checked = true;
+                    }
+                    else
+                    {
+                        checkBox2.Checked = false;
+                        checkBox2.Enabled = false;
+                    }
+                    if (Boolean.Parse(SubstringExtensions.Between(config, "<syncpref>", @"</syncpref>")))
+                    {
+                        checkBox3.Checked = true;
+                    }
+                    else
+                    {
+                        checkBox3.Checked = false;
+                        checkBox3.Enabled = false;
+                    }
+
+                    libraryrepo = SubstringExtensions.Between(config, "<libraryrepo>", @"</libraryrepo>");
+                    mediarepo = SubstringExtensions.Between(config, "<mediarepo>", @"</mediarepo>");
+                    prefrepo = SubstringExtensions.Between(config, "<prefrepo>", @"</prefrepo>");
+
+
+                    
                 }
             }catch (Exception ex)
             {
                 MessageBox.Show("There was an error connecting to the server.  Please check the URL and your connection and try again.  Technical details: " + Environment.NewLine + ex.Message.ToString());
             }
         }
+    }
+}
+static class SubstringExtensions
+{
+    /// <summary>
+    /// Get string value between [first] a and [last] b.
+    /// </summary>
+    public static string Between(this string value, string a, string b)
+    {
+        int posA = value.IndexOf(a);
+        int posB = value.LastIndexOf(b);
+        if (posA == -1)
+        {
+            return "";
+        }
+        if (posB == -1)
+        {
+            return "";
+        }
+        int adjustedPosA = posA + a.Length;
+        if (adjustedPosA >= posB)
+        {
+            return "";
+        }
+        return value.Substring(adjustedPosA, posB - adjustedPosA);
+    }
+
+    /// <summary>
+    /// Get string value after [first] a.
+    /// </summary>
+    public static string Before(this string value, string a)
+    {
+        int posA = value.IndexOf(a);
+        if (posA == -1)
+        {
+            return "";
+        }
+        return value.Substring(0, posA);
+    }
+
+    /// <summary>
+    /// Get string value after [last] a.
+    /// </summary>
+    public static string After(this string value, string a)
+    {
+        int posA = value.LastIndexOf(a);
+        if (posA == -1)
+        {
+            return "";
+        }
+        int adjustedPosA = posA + a.Length;
+        if (adjustedPosA >= value.Length)
+        {
+            return "";
+        }
+        return value.Substring(adjustedPosA);
     }
 }
